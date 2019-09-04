7aba7ee9a0b078222502cc3e89700a6e5e9492da
sangharshasureshranpise <sangharsharanpise408@gmail.com>
2019-09-04 23:42:15 +0530

    [Sangharsha] Add.Checking Equality of one occurence event probability

--- src/test/java/com/thoughtworks/bootcamp/Probability/ProbabilityTest.java
+++ src/test/java/com/thoughtworks/bootcamp/Probability/ProbabilityTest.java
@@ -1,29 +1,37 @@
 package com.thoughtworks.bootcamp.Probability;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ProbabilityTest {
+    private static Probability imposibleEvents;
+    private static Probability certainEvents;
 
+    @BeforeAll
+    static void setUp(){
+        imposibleEvents=new Probability(0.0f);
+        certainEvents=new Probability(1.0f);
+    }
     @Test
     void givenProbabilityOfTwoEventIsZero_whenCheckEquality_thenShouldReturnTrue() {
-        Probability imposibleEvents=new Probability(0.0f);
         assertEquals(imposibleEvents, imposibleEvents);
     }
 
     @Test
     void givenProbabilityOfEventIsOneAndZero_whenCheckEquality_thenShouldReturnTrue() {
-        Probability imposibleEvents=new Probability(0.0f);
-        Probability certainEvents=new Probability(1.0f);
         assertNotEquals(imposibleEvents, certainEvents);
     }
 
     @Test
     void givenZeroOccurenceProbability_whenCheckEquality_thenShouldReturnOne() {
-        Probability imposibleEvents=new Probability(0.0f);
-        Probability certainEvents=new Probability(1.0f);
         assertEquals(certainEvents,imposibleEvents.not());
+    }
+
+    @Test
+    void givenOneOccurenceProbability_whenCheckEquality_thenShouldReturnZero() {
+        assertEquals(certainEvents.not(),imposibleEvents);
     }
 }
