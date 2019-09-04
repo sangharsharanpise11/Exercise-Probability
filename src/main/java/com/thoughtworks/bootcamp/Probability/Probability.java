a2d3eaad114e554fcd7f77b6ce2676609331b7b7
sangharshasureshranpise <sangharsharanpise408@gmail.com>
2019-09-04 23:35:41 +0530

    [Sangharsha] Add.Checking Equality of zero occurence event probability

--- src/main/java/com/thoughtworks/bootcamp/Probability/Probability.java
+++ src/main/java/com/thoughtworks/bootcamp/Probability/Probability.java
@@ -3,15 +3,16 @@ package com.thoughtworks.bootcamp.Probability;
 public class Probability {
     private final float value;
 
-    Probability(float value)
-    {
+    Probability(float value) {
         this.value=value;
     }
 
-    public boolean equals(Object object)
-    {
+    public boolean equals(Object object) {
         Probability that= (Probability) object;
         return this.value==that.value;
     }
 
+    public Probability not() {
+        return new Probability(1.0f-value);
+    }
 }
--- src/test/java/com/thoughtworks/bootcamp/Probability/ProbabilityTest.java
+++ src/test/java/com/thoughtworks/bootcamp/Probability/ProbabilityTest.java
@@ -16,8 +16,14 @@ public class ProbabilityTest {
     @Test
     void givenProbabilityOfEventIsOneAndZero_whenCheckEquality_thenShouldReturnTrue() {
         Probability imposibleEvents=new Probability(0.0f);
-        Probability certainEvents=new Probability(0.1f);
+        Probability certainEvents=new Probability(1.0f);
         assertNotEquals(imposibleEvents, certainEvents);
     }
 
+    @Test
+    void givenZeroOccurenceProbability_whenCheckEquality_thenShouldReturnOne() {
+        Probability imposibleEvents=new Probability(0.0f);
+        Probability certainEvents=new Probability(1.0f);
+        assertEquals(certainEvents,imposibleEvents.not());
+    }
 }
